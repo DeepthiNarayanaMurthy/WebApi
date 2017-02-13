b40b485eea588b058a17dae03db4a57c7a4d7e16
DeepthiNarayanaMurthy <deepthi.narayanamurthy@tieto.com>
2017-02-13 13:11:33 +0530

    first commit

---
+++ WebApi/Controllers/WebApiController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Web;
+using System.Web.Http;
+
+
+namespace WebApi.Controllers
+{
+    public class WebApiController : ApiController
+    {
+        HttpClient client = new HttpClient();
+        public static string accessToken { get; set; }
+        // GET: api/Api
+        [Route("Authenticate")]
+        public HttpResponseMessage Get()
+        {
+            string URL = "https://login.live.com/oauth20_authorize.srf";
+            string urlParameters = "?client_id=c65a0efe-ef12-4bbc-9255-f965789f2522 &scope=wl.basic onedrive.readwrite &response_type=code&redirect_uri=http://localhost:11942/uploadpage.html";
+            client.BaseAddress = new Uri(URL);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = client.GetAsync(urlParameters).Result;
+            return response;
+        }
+
+        //GET: api/Api/5
+        [Route("GetFiles")]
+        public HttpResponseMessage Get([FromBody]string access_Token)
+        {
+            //string[] word = id.Split('"');
+            //accessToken = word[1];
+            string URL = "https://api.onedrive.com/v1.0/drive/items/root:/Favorites/:";
+            string urlParameters = "?expand=children&access_token=" + accessToken;
+            client.BaseAddress = new Uri(URL);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = client.GetAsync(urlParameters).Result;
+            return response;
+        }
+
+        // POST: api/Api
+        [Route("Redeem")]
+        public string Post([FromBody]string value)
+        {
+            var request = (HttpWebRequest)WebRequest.Create("https://login.live.com/oauth20_token.srf");
+            
+            var postData="client_id=c65a0efe-ef12-4bbc-9255-f965789f2522";
+            postData+="&redirect_uri=http://localhost:11942/uploadpage.html";
+            postData += "&client_secret=Fdbug7pJtcxMxfHtN89AWrs";
+            postData += "&code="+ value;
+            postData += "&grant_type=authorization_code";
+            var data = Encoding.ASCII.GetBytes(postData);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = data.Length;
+
+            using (var stream = request.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+
+            var response = (HttpWebResponse)request.GetResponse();
+            StreamReader sr = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+            string html = sr.ReadToEnd();
+            string[] word1 = html.Split(',');
+            string[] word2 = word1[3].Split(':');
+            string[] word3 = word2[1].Split('"');
+            accessToken = word3[1];
+            //Stream receiveStream = response.GetResponseStream();
+            //StreamReader readStream = new StreamReader(receiveStream);
+            //var serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
+            //var jsonObject = serializer.DeserializeObject(readStream.ReadToEnd());
+            return accessToken;
+        }
+
+        // PUT: api/Api/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Api/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApi/Script.js
@@ -0,0 +1,91 @@
+﻿ $(document).ready(function () {
+    $("#authenticate").click(function () {
+        $.ajax({
+            url: "http://localhost:11942/Authenticate",
+            type: 'GET',
+            xhrFields: { 'withCredentials': true },
+            crossDomain: true,
+            headers: {
+                'Access-Control-Allow-Origin': 'http://localhost:11942',
+                'Access-Control-Allow-Origin': '*'
+            },
+            success: function (response) {
+                console.log(response);
+                window.location = "https://login.live.com/oauth20_authorize.srf?client_id=c65a0efe-ef12-4bbc-9255-f965789f2522 &scope=wl.basic onedrive.readwrite &response_type=code&redirect_uri=http://localhost:11942/uploadpage.html";
+            },
+            error: function (response) {
+                console.log("asaax");
+                //console.log(response);
+            }
+        });
+    })
+ });
+ $.extend({
+             getUrlVars: function(){
+                 var vars = [], hash;
+                 var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+                 for(var i = 0; i < hashes.length; i++)
+                 {
+                     hash = hashes[i].split('=');
+                     vars.push(hash[0]);
+                     vars[hash[0]] = hash[1];
+                 }
+                 return vars;
+             },
+     getUrlVar: function(name){
+         return $.getUrlVars()[name];
+     }
+ });
+ var accessToken;
+ $(document).ready(function () {
+     $("#Redeem").click(function () {
+         $.ajax({
+             url: "http://localhost:11942/Redeem",
+             type: 'POST',
+             xhrFields: { withCredentials: true },
+             contentType: 'application/json',
+             dataType: 'json',
+             //crossDomain: true,
+             data: JSON.stringify($.getUrlVar('code')),
+             success: function (response) {
+                 console.log(response);
+                 //var temp = JSON.stringify(response.access_token);
+                 accessToken = response;
+             },
+             error: function (response) {
+                 console.log(response);
+             }
+         });
+     })
+ });
+
+
+ $(document).ready(function () {
+     $("#Getfiles").click(function () {
+         console.log(accessToken);
+         $.ajax({
+             url: "http://localhost:11942/GetFiles",
+             type: 'GET',
+             xhrFields: { 'withCredentials': true },
+             contentType: 'application/json',
+             dataType: 'json',
+             success: function (response) {
+                 console.log(response.children);
+                 folders = response.children;
+                 var buttons = new Array();
+
+                 for (i = 0; i < folders.length; i++) {
+                     document.write('<a href="javascript:;" onclick="displayContent('+"'"+folders[i].id+"'"+')" data-theme="b" data-role="button" >' + folders[i].name + '</a></br>');
+
+                 }
+             },
+             error: function (response) {
+                 console.log(response.RequestMessage);
+                 //console.log(response);
+             }
+         });
+     })
+ });
+ function displayContent(item_id) {
+     console.log(item_id);
+ };
