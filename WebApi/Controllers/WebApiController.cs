d4314a243fc50c46d0dab8356b271469169b7444
DeepthiNarayanaMurthy <deepthi.narayanamurthy@tieto.com>
2017-02-15 15:39:22 +0530

    Second commit

--- WebApi/Controllers/WebApiController.cs
+++ WebApi/Controllers/WebApiController.cs
@@ -7,8 +7,10 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
+using System.Runtime.Serialization;
 
 
 namespace WebApi.Controllers
@@ -32,7 +34,7 @@ namespace WebApi.Controllers
 
         //GET: api/Api/5
         [Route("GetFiles")]
-        public HttpResponseMessage Get([FromBody]string access_Token)
+        public HttpResponseMessage Get([FromBody]int access_Token)
         {
             //string[] word = id.Split('"');
             //accessToken = word[1];
@@ -45,12 +47,22 @@ namespace WebApi.Controllers
             return response;
         }
 
+        [Route("Download/{item_id}")]
+        public HttpResponseMessage Get([FromUri] string item_id)
+        {
+            string URL = "https://api.onedrive.com/v1.0/drive/items/"+item_id+"/content";
+            string urlParameters = "?access_token=" + accessToken;
+            client.BaseAddress = new Uri(URL);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = client.GetAsync(urlParameters).Result;
+            return response;
+        }
         // POST: api/Api
         [Route("Redeem")]
         public string Post([FromBody]string value)
         {
             var request = (HttpWebRequest)WebRequest.Create("https://login.live.com/oauth20_token.srf");
-            
             var postData="client_id=c65a0efe-ef12-4bbc-9255-f965789f2522";
             postData+="&redirect_uri=http://localhost:11942/uploadpage.html";
             postData += "&client_secret=Fdbug7pJtcxMxfHtN89AWrs";
@@ -66,8 +78,8 @@ namespace WebApi.Controllers
                 stream.Write(data, 0, data.Length);
             }
 
-            var response = (HttpWebResponse)request.GetResponse();
-            StreamReader sr = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+            var response1 = (HttpWebResponse)request.GetResponse();
+            StreamReader sr = new StreamReader(response1.GetResponseStream(), Encoding.UTF8);
             string html = sr.ReadToEnd();
             string[] word1 = html.Split(',');
             string[] word2 = word1[3].Split(':');
@@ -80,11 +92,45 @@ namespace WebApi.Controllers
             return accessToken;
         }
 
-        // PUT: api/Api/5
-        public void Put(int id, [FromBody]string value)
+        [HttpPut]
+        [Route("Upload")]
+        public async Task<string> Put()
         {
-        }
+            if (!Request.Content.IsMimeMultipartContent())
+            {
+                return "The request doesn't contain valid content!";
+            }
+            var request = (HttpWebRequest)WebRequest.Create("https://api.onedrive.com/v1.0/drive/root:/Favorites/DL.pdf:/content?access_token=" + accessToken);
+            request.Method = "PUT";
+            request.ContentType = "application/*";
+            try
+            {
+                var provider = new MultipartMemoryStreamProvider();
+                await Request.Content.ReadAsMultipartAsync(provider);
+                foreach (var file in provider.Contents)
+                {
+                    byte[] bytes;
+                    var dataStream = await file.ReadAsStreamAsync();
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        dataStream.CopyTo(ms);
+                        bytes= ms.ToArray();
+                    }
 
+                    request.ContentLength = dataStream.Length;
+                    Stream data= request.GetRequestStream();
+                    data.Write(bytes, 0, bytes.Length);
+                    data.Close();
+                }
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                string returnString = response.StatusCode.ToString();
+                return returnString;
+            }
+            catch (Exception e)
+            {
+                return "error";
+            }
+        }
         // DELETE: api/Api/5
         public void Delete(int id)
         {
--- WebApi/Script.js
+++ WebApi/Script.js
@@ -12,6 +12,7 @@
             success: function (response) {
                 console.log(response);
                 window.location = "https://login.live.com/oauth20_authorize.srf?client_id=c65a0efe-ef12-4bbc-9255-f965789f2522 &scope=wl.basic onedrive.readwrite &response_type=code&redirect_uri=http://localhost:11942/uploadpage.html";
+                console.log(response);
             },
             error: function (response) {
                 console.log("asaax");
@@ -48,7 +49,7 @@
              //crossDomain: true,
              data: JSON.stringify($.getUrlVar('code')),
              success: function (response) {
-                 console.log(response);
+//                 console.log(response);
                  //var temp = JSON.stringify(response.access_token);
                  accessToken = response;
              },
@@ -62,7 +63,7 @@
 
  $(document).ready(function () {
      $("#Getfiles").click(function () {
-         console.log(accessToken);
+       //  console.log(accessToken);
          $.ajax({
              url: "http://localhost:11942/GetFiles",
              type: 'GET',
@@ -70,7 +71,8 @@
              contentType: 'application/json',
              dataType: 'json',
              success: function (response) {
-                 console.log(response.children);
+                 //console.log("hgcghv");
+                 //console.log(response.StatusCode);
                  folders = response.children;
                  var buttons = new Array();
 
@@ -80,12 +82,71 @@
                  }
              },
              error: function (response) {
-                 console.log(response.RequestMessage);
+                 console.log("gfgvjh");
+                 console.log(response.status);
+                 if (response.status == 401)
+                     window.location = "http://localhost:11942/frontpage.html";
+                 console.log(response);
                  //console.log(response);
              }
          });
      })
  });
+
+
  function displayContent(item_id) {
      console.log(item_id);
+     $.ajax({
+         url: "http://localhost:11942/Download/"+item_id,
+         type: 'GET',
+         xhrFields: { 'withCredentials': true },
+         contentType: 'application/json',
+         success: function (response) {
+             //console.log("hgcghv");
+             //console.log(response);
+             var downloadUrl = response['@content.downloadUrl.downloadUrl'];
+             console.log(downloadUrl);
+             console.log("gvghvj");
+         },
+         error: function (response) {
+             console.log("gfgvjh");
+             console.log(response);
+             //console.log(response);
+         }
+     });
  };
+
+
+ $(document).ready(function () {
+     $('#txtUploadFile').on('change', function (e) {
+         var files = e.target.files;
+         console.log("gvgvm");
+         if (files.length > 0) {
+             if (window.FormData !== undefined) {
+                 var data = new FormData();
+                 for (var x = 0; x < files.length; x++) {
+                     data.append("file" + x, files[x]);
+                 }
+
+                 $.ajax({
+                     type: "PUT",
+                     url: 'http://localhost:11942/Upload',
+                     contentType: false,
+                     processData: false,
+                     data: data,
+                     success: function (result) {
+                         console.log(result);
+                     },
+                     error: function (xhr, status, p3, p4) {
+                         var err = "Error " + " " + status + " " + p3 + " " + p4;
+                         if (xhr.responseText && xhr.responseText[0] == "{")
+                             err = JSON.parse(xhr.responseText).Message;
+                         console.log(err);
+                     }
+                 });
+             } else {
+                 alert("This browser doesn't support HTML5 file uploads!");
+             }
+         }
+     });
+ });
